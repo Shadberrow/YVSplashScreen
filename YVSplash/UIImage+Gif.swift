a39ba3f43c754546256ba3ba9a9913250eb9164e
Yevhenii <veret.evg@gmail.com>
2017-07-30 19:20:51 +0300

    first commit

---
+++ YVSplash/UIImage+Gif.swift
@@ -0,0 +1,9 @@
+//
+//  UIImage+Gif.swift
+//  YVSplash
+//
+//  Created by Yevhenii Veretennikov on 7/30/17.
+//  Copyright © 2017 CREATE COURAGE. All rights reserved.
+//
+
+import Foundation
---
+++ YVSplash/YVSplashView.swift
@@ -0,0 +1,15 @@
+//
+//  YVSplash.swift
+//  YVSplash
+//
+//  Created by Yevhenii Veretennikov on 7/30/17.
+//  Copyright © 2017 CREATE COURAGE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class YVSplashView: UIView {
+    
+}
